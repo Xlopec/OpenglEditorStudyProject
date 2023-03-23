8be10714f42b3ae38ef6be4a7f162f785f1aac7d
Maksym Oliinyk <maksimolejn720@gmail.com>
2023-03-23 14:27:54 +0200

    Refactored OpenGL transformations

--- app/src/main/java/com/epam/opengl/edu/ui/gl/TintTransformation.kt
+++ app/src/main/java/com/epam/opengl/edu/ui/gl/TintTransformation.kt
@@ -3,7 +3,7 @@ package com.epam.opengl.edu.ui.gl
 import android.content.Context
 import android.opengl.GLES31
 import com.epam.opengl.edu.R
-import com.epam.opengl.edu.model.Tint
+import com.epam.opengl.edu.model.Transformations
 import java.nio.FloatBuffer
 import javax.microedition.khronos.opengles.GL
 
@@ -11,7 +11,7 @@ class TintTransformation(
     private val context: Context,
     private val verticesCoordinates: FloatBuffer,
     private val textureCoordinates: FloatBuffer,
-) {
+) : OpenglTransformation {
 
     context (GL)
     private val program by lazy {
@@ -19,12 +19,12 @@ class TintTransformation(
     }
 
     context (GL)
-    fun render(
-        tint: Tint,
-        targetFbo: Int,
-        sourceTexture: Int,
+    override fun draw(
+        transformations: Transformations,
+        fbo: Int,
+        texture: Int,
     ) {
-        GLES31.glBindFramebuffer(GLES31.GL_FRAMEBUFFER, targetFbo)
+        GLES31.glBindFramebuffer(GLES31.GL_FRAMEBUFFER, fbo)
         GLES31.glClear(GLES31.GL_COLOR_BUFFER_BIT)
         GLES31.glUseProgram(program)
         val positionHandle = GLES31.glGetAttribLocation(program, "aPosition")
@@ -32,8 +32,8 @@ class TintTransformation(
         val tintHandle = GLES31.glGetUniformLocation(program, "tint")
         GLES31.glVertexAttribPointer(texturePositionHandle, 2, GLES31.GL_FLOAT, false, 0, textureCoordinates)
         GLES31.glEnableVertexAttribArray(texturePositionHandle)
-        GLES31.glBindTexture(GLES31.GL_TEXTURE_2D, sourceTexture)
-        GLES31.glUniform1f(tintHandle, tint.value)
+        GLES31.glBindTexture(GLES31.GL_TEXTURE_2D, texture)
+        GLES31.glUniform1f(tintHandle, transformations.tint.value)
         GLES31.glVertexAttribPointer(positionHandle, 2, GLES31.GL_FLOAT, false, 0, verticesCoordinates)
         GLES31.glEnableVertexAttribArray(positionHandle)
         GLES31.glDrawArrays(GLES31.GL_TRIANGLE_STRIP, 0, 4)
