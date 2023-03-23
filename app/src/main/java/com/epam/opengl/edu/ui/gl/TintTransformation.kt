b2979a9d4b66ce1947e9e543b870d0bd897861dd
Maksym Oliinyk <maksimolejn720@gmail.com>
2023-03-23 14:29:24 +0200

    Textures ping-pong improvements

--- app/src/main/java/com/epam/opengl/edu/ui/gl/TintTransformation.kt
+++ app/src/main/java/com/epam/opengl/edu/ui/gl/TintTransformation.kt
@@ -21,10 +21,10 @@ class TintTransformation(
     context (GL)
     fun render(
         tint: Tint,
-        fbo: Int,
-        texture: Int,
+        targetFbo: Int,
+        sourceTexture: Int,
     ) {
-        GLES31.glBindFramebuffer(GLES31.GL_FRAMEBUFFER, fbo)
+        GLES31.glBindFramebuffer(GLES31.GL_FRAMEBUFFER, targetFbo)
         GLES31.glClear(GLES31.GL_COLOR_BUFFER_BIT)
         GLES31.glUseProgram(program)
         val positionHandle = GLES31.glGetAttribLocation(program, "aPosition")
@@ -32,7 +32,7 @@ class TintTransformation(
         val tintHandle = GLES31.glGetUniformLocation(program, "tint")
         GLES31.glVertexAttribPointer(texturePositionHandle, 2, GLES31.GL_FLOAT, false, 0, textureCoordinates)
         GLES31.glEnableVertexAttribArray(texturePositionHandle)
-        GLES31.glBindTexture(GLES31.GL_TEXTURE_2D, texture)
+        GLES31.glBindTexture(GLES31.GL_TEXTURE_2D, sourceTexture)
         GLES31.glUniform1f(tintHandle, tint.value)
         GLES31.glVertexAttribPointer(positionHandle, 2, GLES31.GL_FLOAT, false, 0, verticesCoordinates)
         GLES31.glEnableVertexAttribArray(positionHandle)
